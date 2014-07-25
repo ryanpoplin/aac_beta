42f3dc3be56570ba636c2919ebe2cf82b3630fb9
Byrdann Fox <mobileupstate@gmail.com>
2014-07-25 14:15:29 -0400

    Updates...

---
+++ aac_beta/aac_beta/ECTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ECTAppDelegate.h
+//  aac_beta
+//
+//  Created by Byrdann Fox on 7/25/14.
+//  Copyright (c) 2014 com.excepapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ aac_beta/aac_beta/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  aac_beta
+//
+//  Created by Byrdann Fox on 7/25/14.
+//  Copyright (c) 2014 com.excepapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ECTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ECTAppDelegate class]));
+    }
+}
