503c5f1fe19e8c6985c71b77209c9080f5c6629c
Byrdann Fox <mobileupstate@gmail.com>
2014-08-14 11:47:05 -0400

    Updates...

---
+++ aac_beta/aac_beta/ECTViewController.m
@@ -0,0 +1,26 @@
+//
+//  ECTViewController.m
+//  aac_beta
+//
+//  Created by Byrdann Fox on 8/14/14.
+//  Copyright (c) 2014 com.excepapps. All rights reserved.
+//
+
+#import "ECTViewController.h"
+
+@interface ECTViewController ()
+
+@end
+
+@implementation ECTViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+@end
