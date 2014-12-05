41b03522ef4492fd95272ee036752f5b543b2fe2
Byrdann Fox <mobileupstate@gmail.com>
2014-12-05 16:35:17 -0500

    Updates...

---
+++ aac_beta/ECTViewController.h
@@ -0,0 +1,13 @@
+//
+//  ECTViewController.h
+//  aac_beta
+//
+//  Created by Byrdann Fox on 8/14/14.
+//  Copyright (c) 2014 com.excepapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECTViewController : UIViewController
+
+@end
---
+++ aac_beta/ECTViewController.m
@@ -0,0 +1,13 @@
+//
+//  ECTViewController.m
+//  aac_beta
+//
+//  Created by Byrdann Fox on 8/14/14.
+//  Copyright (c) 2014 com.excepapps. All rights reserved.
+//
+
+#import "ECTViewController.h"
+
+@implementation ECTViewController
+
+@end
